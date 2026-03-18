baaf655cc32de541747d8a5d6cca35e2a2b500b3
Aditya425148 <aadi100right@gmail.com>
2026-03-18 21:17:20 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,7 +6,7 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import simon from "./simon.png";
+import civic from "./civic.png";
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
@@ -29,7 +29,7 @@ import AgriTech from "./AgriTech.png";
 
 
 export {
-  simon,
+  civic,
   medicate,
   AgriTech,
   heroVideo,
